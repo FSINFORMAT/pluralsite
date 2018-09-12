a44df75b22c9217915a7486636e04886ca86a4df
fariborzsaidi <fs.informat@gmail.com>
2018-09-12 23:48:47 +0200

    new methode

--- maven-webapp/src/main/java/org/paumard/model/luk.java
+++ maven-webapp/src/main/java/org/paumard/model/luk.java
@@ -1,5 +1,20 @@
 package org.paumard.model;
 
 public class luk {
-private String name;
+	private String name;
+
+	@Override
+	public String toString() {
+		return "luk [name=" + name + "]";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
 }
